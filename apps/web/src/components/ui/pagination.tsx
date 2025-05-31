c4e36fc542bb0764df4adf2c62df42b35df57e3d
Muhammad Yusuf <yusuf@iamyuu.dev>
2025-05-31 23:17:12 +0700

    style: fix simple pagination

--- apps/web/src/components/ui/pagination.tsx
+++ apps/web/src/components/ui/pagination.tsx
@@ -13,10 +13,10 @@ export function SimplePagination(props: SimplePaginationProps) {
   return (
     <div className="flex items-center space-x-2 justify-end">
       <Button variant="outline" onClick={props.onPrev} disabled={!props.hasPrev}>
-        <ChevronLeft className="mr-2 h-4 w-4" />
+        <ChevronLeft className="h-4 w-4" />
       </Button>
       <Button variant="outline" onClick={props.onNext} disabled={!props.hasNext}>
-        <ChevronRight className="ml-2 h-4 w-4" />
+        <ChevronRight className="h-4 w-4" />
       </Button>
     </div>
   )
